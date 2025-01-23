649d9f51f0c08972c78058eaf27e7894c376dbeb
PalmPalmPalmPalmPalm <93920339+Palmkonde@users.noreply.github.com>
2025-01-23 10:21:06 +0700

    change colors

--- main.py
+++ main.py
@@ -11,30 +11,40 @@ class PlotGraph:
         self.range = range
         self.x = np.linspace(start=range[0], stop=range[1], num=amount)
         
+        plt.title("plotting 3 function in the graph")
+
         plt.ylim(-10, 10)
+        plt.xlim(-10, 10)
+
+        plt.ylabel("y value")
+        plt.xlabel("x value")
     
     def f1(self) -> None:
         y = np.power(self.x, 2) 
+        fmt = "g"
         
-        plt.plot(self.x, y)
+        plt.plot(self.x, y, fmt, label="x^2")
         
         logger.info("this is y: %s", y)
     
     def f2(self) -> None:
         y = self.x * np.sin(2 * self.x)
+        fmt = "r"
         
-        plt.plot(self.x, y)
+        plt.plot(self.x, y, fmt, label="x*sin(2*x)")
 
         logger.info("this is y: %s", y)
         
     def f3(self) -> None:
         y = np.arctan(self.x)
+        fmt = "b"
         
-        plt.plot(self.x, y)
+        plt.plot(self.x, y, fmt, label="argtg(x)")
 
         logger.info("this is y: %s", y)
     
     def show(self) -> None:
+        plt.legend()
         plt.show()
         
 
