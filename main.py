a0e6bf3a4303c5ff4a08a66893b410773bfc6c74
PalmPalmPalmPalmPalm <93920339+Palmkonde@users.noreply.github.com>
2025-01-23 09:54:45 +0700

    1a done

---
+++ main.py
@@ -0,0 +1,29 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from typing import Tuple
+import logging
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger()
+
+class PlotGraph:
+    def __init__(self, range: Tuple[int, int]=(0, 10), amount: int=100) -> None:
+        self.range = range
+        self.x = np.linspace(start=range[0], stop=range[1], num=amount)
+    
+    def f1(self) -> None:
+        y = np.power(self.x, 2) 
+        
+        plt.plot(self.x, y)
+        plt.show()
+        
+        logger.info("this is y: %s", y)
+        
+    
+    def plot(self) -> None:
+        pass
+        
+
+if __name__ == "__main__":
+    pg = PlotGraph(range=(-10, 10))
+    pg.f1()
