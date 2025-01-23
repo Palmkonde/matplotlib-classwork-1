efc32c45962a7b9fe68fc11003379aa31b5105a6
PalmPalmPalmPalmPalm <93920339+Palmkonde@users.noreply.github.com>
2025-01-23 13:09:50 +0700

    Histrogram

--- bar_hist.py
+++ bar_hist.py
@@ -6,7 +6,7 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger()
 
 bar_data = np.genfromtxt('values_for_bars.csv', delimiter=',')
-hist_data = np.genfromtxt('values_for_bars.csv', delimiter=',')
+hist_data = np.genfromtxt('values_for_hist.csv', delimiter=',')
 
 
 def bar(data: np.ndarray) -> None:
@@ -18,6 +18,12 @@ def bar(data: np.ndarray) -> None:
     plt.xticks(unique_data)
     plt.show()
 
+def hist(data: np.ndarray) -> None:
+    logger.info("%s", hist_data)
+    
+    plt.hist(data)
+    plt.show()
+
 
 if __name__ == "__main__":
-    bar(bar_data)
+    hist(hist_data)
